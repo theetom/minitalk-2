da62bf8ac152009856f4ecf2da73fa21154e4853
tomazz casa <etom@PcdoTomaz>
2024-11-07 11:36:11 +0000

    olha que vcem ai

--- client.c
+++ client.c
@@ -3,24 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: toferrei <toferrei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 14:39:33 by toferrei          #+#    #+#             */
-/*   Updated: 2024/10/30 14:01:02 by toferrei         ###   ########.fr       */
+/*   Updated: 2024/11/07 02:25:13 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+int	chars_to_binary(int *bin)
+{
+
+}
+
 int	main(int argc, char **argv)
 {
 	char	test[6] = {'0', '1', '0', '1', '0', '1'};
 	pid_t	pid;
+	int		n;
+	int		*bin;
+
 	if (argc != 2)
-		return 0;
+		return (0);
 	pid = atoi(argv[1]);
 	printf("%d", pid);
-	int n = 0;
+	
+	n = chars_to_binary();
 	if (test[n] == '0')
 		kill(pid, SIGUSR1);
 	else
