afb546066211f9ad6c26e5161ee1db72295e2bb0
tomazz casa <etom@PcdoTomaz>
2024-11-14 12:11:43 +0000

    corrections

--- Ft_Printf/ft_putnbr_base_u_pf.c
+++ Ft_Printf/ft_putnbr_base_u_pf.c
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 17:33:07 by toferrei          #+#    #+#             */
-/*   Updated: 2024/05/10 19:10:10 by toferrei         ###   ########.fr       */
+/*   Updated: 2024/11/14 11:59:08 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ size_t	ft_putnbr_base_u_pf(unsigned int nb, char *base)
 
 	x = 0;
 	b = ft_strlen_pf(base);
-	if (nb < 0)
+/* 	if (nb < 0)
 	{
 		x += write(1, "-", 1);
 		nb = -nb;
-	}
+	} */
 	if (nb > b - 1)
 		x += ft_putnbr_base_pf(nb / b, base);
 	x += write(1, &base[nb % b], 1);
